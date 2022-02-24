19b93aee85e41ddf73ff68adc34a8986c3f8a85f
Jackie Tung <jackietung3@gmail.com>
2022-02-24 18:11:16 -0500

    commit tree d38aca8510526a269f28beae5f5206013719acf9

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function superbowlWin(record) {
+   for (const info of record){
+        if (info.result === "W"){
+            return info.year
+        }
+    }
+    // const HHH = record.find(game => game.result === "W")
+    // if (HHH) return HHH.year
+}
+
+// const LastWonGame = record.find(
+//     (element) =>{return element.result == "W"}
+//     );
+
+// function superbowlWin(element){
+//     return element.result == "W"
+// }
+
